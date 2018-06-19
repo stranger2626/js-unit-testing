9b82c36628718e1c83225343090c539b57621eaf
unknown <stranger2626@gmail.com>
2018-06-19 10:44:42 +0300

    updated some dependencies and fixed a few typos

--- mocha_tests/mochaTests.js
+++ mocha_tests/mochaTests.js
@@ -14,33 +14,33 @@ describe('Array', function() {
     });
 });
 let myPromise = new Promise((resolve, reject) => {
-    setTimeout(function(){
+    setTimeout(function() {
         resolve('Success!');
     }, 250);
 });
 
 describe('ReadData', function() {
-    it('should read data from from properties file', () => {
+    it('should read data from properties file', () => {
         let data = readData('somefile.properties');
         expect(data).to.be.equal('data from properties file');
     });
 
-    it('should read data from from csv file', () => {
+    it('should read data from csv file', () => {
         let data = readData('somefile.csv');
         expect(data).to.be.equal('data from csv file');
     });
 
-    it('should read data from from json file', () => {
+    it('should read data from json file', () => {
         let data = readData('test/test-data/test.json');
         expect(data.data).to.be.equal('data from json file');
     });
 
-    it('should read data from from yml file', () => {
+    it('should read data from yml file', () => {
         let data = readData('somefile.yml');
         expect(data).to.be.equal('data from yml file');
     });
 
-    it('should read data from from xlsx file', () => {
+    it('should read data from xlsx file', () => {
         let data = readData('somefile.xlsx');
         expect(data).to.be.equal('data from xlsx file');
     });
